7c2b1a71bbe7f08128da4f4145631a108a58f697
lukeme <lukeme@users.noreply.github.com>
2014-05-23 10:42:23 +0800

    Create resource.h

---
+++ resource.h
@@ -0,0 +1,25 @@
+//{{NO_DEPENDENCIES}}
+// Microsoft Developer Studio generated include file.
+// Used by php.rc
+//
+#define IDM_ABOUTBOX                    0x0010
+#define IDD_ABOUTBOX                    100
+#define IDS_ABOUTBOX                    101
+#define IDD_PHP_DIALOG                  102
+#define IDR_MAINFRAME                   128
+#define IDR_MENU1                       129
+#define IDC_INPUT                       1001
+#define IDC_OUTPUT                      1002
+#define ID_MENU_EXEC                    32771
+#define ID_MENU_CLEAR                   32772
+
+// Next default values for new objects
+// 
+#ifdef APSTUDIO_INVOKED
+#ifndef APSTUDIO_READONLY_SYMBOLS
+#define _APS_NEXT_RESOURCE_VALUE        130
+#define _APS_NEXT_COMMAND_VALUE         32773
+#define _APS_NEXT_CONTROL_VALUE         1004
+#define _APS_NEXT_SYMED_VALUE           101
+#endif
+#endif
